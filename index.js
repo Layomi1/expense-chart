23841da06a9d5923d09c840fbd11b7514613fef8
Layomi <layomilawore@gmail.com>
2026-08-29 11:45:49 +0100

    implementing expense chart with html, css and javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+const chart = document.querySelector("#chart");
+
+async function getData() {
+  try {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+
+    data.forEach((element) => {
+      const bar = document.createElement("article");
+      bar.classList.add("bar");
+
+      bar.style.height = `${element.amount}%`;
+
+      const currentDay = new Intl.DateTimeFormat("en-US", {
+        weekday: "short",
+      }).format(Date.now());
+
+      if (element.day == currentDay.toLocaleLowerCase()) {
+        bar.style.backgroundColor = "#87bec4";
+      }
+
+      bar.innerHTML = `<span id='day'> ${element.day}
+      </span>`;
+
+      const tooltip = document.createElement("p");
+
+      tooltip.classList.add("tooltip");
+      tooltip.textContent = `$${element.amount}`;
+
+      chart.appendChild(bar);
+      bar.appendChild(tooltip);
+    });
+  } catch (err) {
+    console.log("unable to fetch data!", err);
+  }
+}
+getData();
